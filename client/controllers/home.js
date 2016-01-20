22f949e77e3b2d8565d5c7c50a1185bf54b238b5
rakmsey <raksmey.ny>
2016-01-20 18:01:04 +0700

    search&slide

---
+++ client/controllers/home.js
@@ -0,0 +1,17 @@
+//Searchproduct
+Template.home.helpers({
+    listproducts: function(limit) {
+        return product.find({});
+    },
+    getImage: function(id) {
+        var img = images.findOne({
+            _id: id
+        });
+        if (img) {
+            console.log(img.copies.images.key);
+            return img.copies.images.key;
+        } else {
+            return;
+        }
+    }
+});
